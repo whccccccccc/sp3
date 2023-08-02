92de271e09140d07021b934738296fcc8ce8465d
whc <yixiaoyaotian@163.com>
2023-08-02 22:39:03 +0800

    初步学习web开发

---
+++ boot3-04-web/src/test/java/com/atguigu/boot304web/Boot304WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot304web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot304WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
